1f1da9cac0db371cbdf76784a48cbc0c80340134
Jacqueline-Kelly <jacqueline.kelly.2016@gmail.com>
2023-01-19 12:47:28 +0200

    change sendFile path

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => 
-      res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+      res.sendFile(__dirname, '../', 'frontend/build/index.html'));
 } else {
     app.get('/', (req,res) => {
         res.status(200).json({ message: 'Welcome to the review website'})
